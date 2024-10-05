2c8d2e1b1ffa74221c16f966633de10aadda58d2
ShymaaRagab <102189654+ShymaaRagab@users.noreply.github.com>
2024-10-05 23:59:29 +0300

    Sh-R

--- src/app/blog/blog-card/blog-card.component.ts
+++ src/app/blog/blog-card/blog-card.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BlogService } from '../../../services/blog.service';
+import { Blog } from '../../../interfaces/blog.interface';
 
 @Component({
   selector: 'app-blog-card',
@@ -7,10 +8,31 @@ import { BlogService } from '../../../services/blog.service';
   styleUrl: './blog-card.component.css'
 })
 export class BlogCardComponent {
+
   constructor(public blogs:BlogService){}
   maxLength = 100;
 
+  pageSize = 4;
+  currentPage = 1;
+  paginatedBlog: Blog[]=[];
+
   toggleText(index:number) {
     this.blogs.blogs_data[index].showFullText = !this.blogs.blogs_data[index].showFullText;
+  }
+
+
+  ngOnInit() {
+    this.setPage(1);
+  }
+
+
+  get totalPages() {
+    return Array(Math.ceil(this.blogs.blogs_data.length / this.pageSize)).fill(0).map((x, i) => i + 1);
+  }
+
+  setPage(page: number) {
+    this.currentPage = page;
+    const startIndex = (page - 1) * this.pageSize;
+    this.paginatedBlog = this.blogs.blogs_data.slice(startIndex, startIndex + this.pageSize);
   }
 }
