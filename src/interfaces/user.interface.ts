eb7d6d47415e8d530327f4e1257f06710dca8dd1
ShaimaaNader <shaimaa.itec@gmail.com>
2024-10-15 19:01:43 +0300

    Shaimaa.N

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -2,11 +2,13 @@ export interface User {
   id?:number;
 	user_name?: string;
   email: string;
-	password: string;
+	password?: string;
   photo?:string;
   age?: number;
 	role?: string;
   comments?: string[];
   enrolled?: number[];
   progress?: string[];
+  firstAccess?: string;
+  lastAccess?: string;
 }
